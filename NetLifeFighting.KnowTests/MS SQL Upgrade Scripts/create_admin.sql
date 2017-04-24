b819fbf8300c98e459ce83e5cab4080295a6533f
maksimkuzyanin <maximkusyanin@gmail.com>
2017-04-25 01:12:55 +0400

    скрипт создания администратора

---
+++ NetLifeFighting.KnowTests/MS SQL Upgrade Scripts/create_admin.sql
@@ -0,0 +1,11 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+declare
+	@adminName varchar(200) = 'admin',
+	@pass varchar(200) = 'e00cf25ad42683b3df678c61f42c6bda';--admin1
+
+insert into Person (Nickname, [Password], RoleType) values (@adminName, @pass, 1);
+GO
